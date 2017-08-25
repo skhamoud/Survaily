e66d91bdb7f47dc5144f2988b5c12e51dd3f5f24
Hamoud Khatar <skhamoud@gmail.com>
2017-08-25 17:45:44 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+app.listen(PORT, () => {
+  console.warn("listening on PORT ", PORT);
+});
