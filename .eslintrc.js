883d2d77221c814c9e1673d1b671dcab6616fb89
Hamoud Khatar <skhamoud@gmail.com>
2018-01-15 00:29:36 +0000

    shut linter up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-    "extends": "airbnb"
+  extends: 'airbnb',
+  root: true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+  },
+  rules: {
+    'react/jsx-filename-extension': 0,
+  },
 };
