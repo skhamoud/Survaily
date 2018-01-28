f59376e72f951f3114e3b85276681a451a8f6e6a
Hamoud Khatar <skhamoud@gmail.com>
2018-01-28 16:27:43 +0000

    Add production building configs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const passport = require('passport');
 const mongoose = require('mongoose');
@@ -26,6 +27,16 @@ app.use(cookieSession({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/', router);
+
+if (process.env.NODE_ENV === 'production') {
+  // serve static files
+  app.use(express.static('client/build'));
+
+  // Serve index.html for all unrecognised requests
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
